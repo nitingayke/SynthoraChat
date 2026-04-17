ea64e9fa743064b3097f8168a07aad4a0a19df60
Nitin Gayke <gaykenitin975@gmail.com>
2026-04-17 23:17:34 +0530

    added deployed backend url

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://synthora-chat.vercel.app",
     methods: "*",
     credentials: true,
   }),
