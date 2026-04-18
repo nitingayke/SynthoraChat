02ae79bc3297fb8f8e0f04970abe03c1626f8dca
Nitin Gayke <gaykenitin975@gmail.com>
2026-04-18 11:50:39 +0530

    updated contact info

--- client/src/components/contact/ContactInfo.jsx
+++ client/src/components/contact/ContactInfo.jsx
@@ -29,13 +29,31 @@ export default function ContactInfo() {
                 <p className="font-semibold dark:text-white mb-3">
                     Follow Us
                 </p>
+
                 <div className="flex gap-5">
-                    {[Linkedin, Github, Twitter].map((Icon, i) => (
-                        <Icon
-                            key={i}
-                            className="cursor-pointer hover:scale-110 transition text-gray-600 dark:text-gray-400 hover:text-orange-500 dark:hover:text-[#07C5B9]"
-                        />
-                    ))}
+                    <a
+                        href="https://www.linkedin.com/in/nitin-gayke92/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Linkedin className="cursor-pointer hover:scale-110 transition text-gray-600 dark:text-gray-400 hover:text-orange-500 dark:hover:text-[#07C5B9]" />
+                    </a>
+
+                    <a
+                        href="https://github.com/nitingayke"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Github className="cursor-pointer hover:scale-110 transition text-gray-600 dark:text-gray-400 hover:text-orange-500 dark:hover:text-[#07C5B9]" />
+                    </a>
+
+                    <a
+                        href="https://twitter.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Twitter className="cursor-pointer hover:scale-110 transition text-gray-600 dark:text-gray-400 hover:text-orange-500 dark:hover:text-[#07C5B9]" />
+                    </a>
                 </div>
             </div>
         </div>
